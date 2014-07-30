8a65e8b5555115989e78389581761a151fe539d6
Clark Phillips <clark.r.phillips@gmail.com>
2014-07-30 00:44:19 -0500

    easy to compare trees now

---
+++ Trees.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -f Trees.txt
+echo "python" >> Trees.txt
+cat csadt_python/Tree.txt >> Trees.txt
+echo "" >> Trees.txt
+echo "" >> Trees.txt
+echo "C++" >> Trees.txt
+cat csadt_cpp/adt/Tree.txt >> Trees.txt
+echo "" >> Trees.txt
